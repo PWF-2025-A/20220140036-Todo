147576c7b9ebbaf548fff77410f02209d58536b9
muhfarrasm <muhammadfarrasm@gmail.com>
2025-03-19 05:18:38 +0700

    env, mvc, route, relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,11 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function todos()
+    {
+        return $this->hasMany(Todo::class);
+    }
 }
+
+
